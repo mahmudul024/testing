7e6a444b02f62521acc1edb4d2cde969e2cad0b6
mahmudul024 <mhp016144@gmail.com>
2020-06-21 14:33:49 +0600

    Adding a program that would read data from the user and show a message.

--- Testing.R
+++ Testing.R
@@ -1,3 +1,6 @@
-print("this file was created in Rstudio")
 
-print("And now it resides on Github")
+#this is a program that will read a data from the user and will print a message on the screen.
+
+h <- "Hello"
+your_name <- readline("What is your name?\t\n")
+print(paste(h, your_name))
